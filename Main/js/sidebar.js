7f7dad4c7d006bce5b15b2e1a541643d45a9e3d5
LJS0817 <iam0817jun@gmail.com>
2025-11-26 15:52:34 +0900

    main

---
+++ Main/js/sidebar.js
@@ -0,0 +1,18 @@
+const sidebar = document.getElementById('sidebar')
+
+let curIndex = 0;
+
+function changeView(idx) {
+    if(curIndex > -1) sidebar.children[curIndex].classList.remove("active")
+    curIndex = idx;
+    sidebar.children[curIndex].classList.add("active")
+}
+
+window.onload = () => {
+    for(let i = 0; i < sidebar.children.length; i++) {
+        sidebar.children[i].onclick = () => {
+            changeView(i);
+        }
+    }
+    changeView(0);
+}
