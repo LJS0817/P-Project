c4ae04188f4761b34f458523a67c8bb0d42439fa
LJS0817 <iam0817jun@gmail.com>
2025-12-01 22:58:20 +0900

    edit auth

--- Server/routes/auth/authRouter.js
+++ Server/routes/auth/authRouter.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
-// 로그인 페이지 라우트 /signin
 router.get('/signin', (req, res) => {
     res.render('auth/sign', {authForm:'../../views/auth/signin.ejs'}, (e, content) => {
         res.end(content);
     });
 });
 
-// 회원가입 페이지 라우트 /signup
+router.post('/signin', (req, res) => {
+    return res.status(400).json({
+        success: false,
+        reason: ['id', 'pwd']
+    });
+});
+
+
 router.get('/signup', (req, res) => {
     res.render('auth/sign', {authForm:'../../views/auth/signup'}, (e, content) => {
         res.end(content);
     });
 });
+
+router.post('/create', (req, res) => {
+    res.render('auth/sign', {authForm:'../../views/auth/signin.ejs'}, (e, content) => {
+        res.end(content);
+    });
+});
+
 
 module.exports = router;
